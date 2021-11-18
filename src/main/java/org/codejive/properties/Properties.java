23a36c9b07c90b9354d73e838851b16026185d5a
Tako Schotanus <tako@codejive.org>
2021-11-19 00:29:32 +0100

    feat: Loading from file and map values implemented

--- src/main/java/org/codejive/properties/Properties.java
+++ src/main/java/org/codejive/properties/Properties.java
@@ -7,42 +7,61 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.Map;
+import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Set;
-import java.util.TreeMap;
+import java.util.stream.Collectors;
 
-public class Properties {
-    private final Map<String, String> values = new TreeMap<>();
+public class Properties extends AbstractMap<String, String> {
+    private final LinkedHashMap<String, String> values = new LinkedHashMap<>();
+    private final List<PropertiesParser.Token> tokens = new ArrayList<>();
 
-    public String getProperty(String key) {
-        return getProperty(key, null);
+    @Override
+    public Set<Entry<String, String>> entrySet() {
+        return new AbstractSet<Entry<String, String>>() {
+            @Override
+            public Iterator<Entry<String, String>> iterator() {
+                return new Iterator<Entry<String, String>>() {
+                    Iterator<Entry<String, String>> iter = values.entrySet().iterator();
+
+                    @Override
+                    public boolean hasNext() {
+                        return iter.hasNext();
+                    }
+
+                    @Override
+                    public Entry<String, String> next() {
+                        return iter.next();
+                    }
+
+                    @Override
+                    public void remove() {
+                        // TODO handle remove
+                        iter.remove();
+                    }
+                };
+            }
+
+            @Override
+            public int size() {
+                return values.entrySet().size();
+            }
+        };
     }
 
-    public String getProperty(String key, String defaultValue) {
-        return values.getOrDefault(key, defaultValue);
-    }
-
-    public String setProperty(String key, String value) {
+    @Override
+    public String put(String key, String value) {
+        // TODO handle adds and replaces
         return values.put(key, value);
     }
 
-    public void setProperties(Properties props) {
-        setProperties(props.values);
-    }
-
-    public void setProperties(Map<String, String> props) {
-        values.putAll(props);
-    }
-
-    public boolean containsKey(String key) {
-        return values.containsKey(key);
-    }
-
-    public Set<String> keySet() {
-        return values.keySet();
-    }
-
-    public String remove(String key) {
+    @Override
+    public String remove(Object key) {
+        // TODO handle remove
         return values.remove(key);
     }
 
@@ -51,10 +70,22 @@ public class Properties {
     }
 
     public void load(Reader reader) throws IOException {
+        tokens.clear();
         BufferedReader br =
                 reader instanceof BufferedReader
                         ? (BufferedReader) reader
                         : new BufferedReader(reader);
+        List<PropertiesParser.Token> ts = PropertiesParser.tokens(br)
+                .collect(Collectors.toList());
+        tokens.addAll(ts);
+        String key = null;
+        for (PropertiesParser.Token token : tokens) {
+            if (token.type == PropertiesParser.Type.KEY) {
+                key = token.getText();
+            } else if (token.type == PropertiesParser.Type.VALUE) {
+                values.put(key, token.getText());
+            }
+        }
     }
 
     public static Properties loadProperties(Path file) throws IOException {
---
+++ src/test/java/org/codejive/properties/TestProperties.java
@@ -0,0 +1,26 @@
+package org.codejive.properties;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestProperties {
+
+    @Test
+    void testLoad() throws IOException, URISyntaxException {
+        Path f = Paths.get(getClass().getResource("/test.properties").toURI());
+        Properties p = Properties.loadProperties(f);
+        assertThat(p, aMapWithSize(7));
+        assertThat(p.keySet(), contains("one", "two", "three", " with spaces", "altsep", "multiline", "key.4"));
+        assertThat(p.values(), contains("simple", "value containing spaces", "and escapes\n\t\r\f", "everywhere",
+                "value", "one \n    two  \n\tthree", "\u1234"));
+    }
+}
