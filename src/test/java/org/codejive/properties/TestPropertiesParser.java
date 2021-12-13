2eb8ce02056a8a657b8225cfab7d0a20870dad1b
Tako Schotanus <tako@codejive.org>
2021-12-13 12:17:53 +0100

    test: Switched from Hamcrest to AssertJ

--- src/test/java/org/codejive/properties/TestPropertiesParser.java
+++ src/test/java/org/codejive/properties/TestPropertiesParser.java
@@ -1,11 +1,9 @@
 package org.codejive.properties;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.assertj.core.api.Assertions.*;
 
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -39,60 +37,52 @@ public class TestPropertiesParser {
         StringReader rdr = new StringReader(props);
         List<PropertiesParser.Token> tokens =
                 PropertiesParser.tokens(rdr).collect(Collectors.toList());
-        assertThat(
-                tokens,
-                equalTo(
-                        Arrays.asList(
-                                new Token(Type.COMMENT, "#comment1"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.COMMENT, "#  comment2   "),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.COMMENT, "! comment3"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.KEY, "one"),
-                                new Token(Type.SEPARATOR, "="),
-                                new Token(Type.VALUE, "simple"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.KEY, "two"),
-                                new Token(Type.SEPARATOR, "="),
-                                new Token(Type.VALUE, "value containing spaces"),
-                                new Token(Type.WHITESPACE, "\n\r"),
-                                new Token(Type.COMMENT, "# another comment"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.COMMENT, "! and a comment"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.COMMENT, "! block"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.KEY, "three"),
-                                new Token(Type.SEPARATOR, "="),
-                                new Token(
-                                        Type.VALUE,
-                                        "and escapes\\n\\t\\r\\f",
-                                        "and escapes\n\t\r\f"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.WHITESPACE, "  "),
-                                new Token(Type.KEY, "\\ with\\ spaces", " with spaces"),
-                                new Token(Type.SEPARATOR, "   =    "),
-                                new Token(Type.VALUE, "everywhere  "),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.KEY, "altsep"),
-                                new Token(Type.SEPARATOR, ":"),
-                                new Token(Type.VALUE, "value"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.KEY, "multiline"),
-                                new Token(Type.SEPARATOR, " = "),
-                                new Token(
-                                        Type.VALUE,
-                                        "one \\\n    two  \\\n\r\tthree",
-                                        "one two  three"),
-                                new Token(Type.WHITESPACE, "\n"),
-                                new Token(Type.KEY, "key.4"),
-                                new Token(Type.SEPARATOR, " = "),
-                                new Token(Type.VALUE, "\\u1234", "\u1234"),
-                                new Token(Type.WHITESPACE, "\n\r"),
-                                new Token(Type.WHITESPACE, "  "),
-                                new Token(Type.COMMENT, "# final comment"))));
+        assertThat(tokens)
+                .containsExactly(
+                        new Token(Type.COMMENT, "#comment1"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.COMMENT, "#  comment2   "),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.COMMENT, "! comment3"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.KEY, "one"),
+                        new Token(Type.SEPARATOR, "="),
+                        new Token(Type.VALUE, "simple"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.KEY, "two"),
+                        new Token(Type.SEPARATOR, "="),
+                        new Token(Type.VALUE, "value containing spaces"),
+                        new Token(Type.WHITESPACE, "\n\r"),
+                        new Token(Type.COMMENT, "# another comment"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.COMMENT, "! and a comment"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.COMMENT, "! block"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.KEY, "three"),
+                        new Token(Type.SEPARATOR, "="),
+                        new Token(Type.VALUE, "and escapes\\n\\t\\r\\f", "and escapes\n\t\r\f"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.WHITESPACE, "  "),
+                        new Token(Type.KEY, "\\ with\\ spaces", " with spaces"),
+                        new Token(Type.SEPARATOR, "   =    "),
+                        new Token(Type.VALUE, "everywhere  "),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.KEY, "altsep"),
+                        new Token(Type.SEPARATOR, ":"),
+                        new Token(Type.VALUE, "value"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.KEY, "multiline"),
+                        new Token(Type.SEPARATOR, " = "),
+                        new Token(Type.VALUE, "one \\\n    two  \\\n\r\tthree", "one two  three"),
+                        new Token(Type.WHITESPACE, "\n"),
+                        new Token(Type.KEY, "key.4"),
+                        new Token(Type.SEPARATOR, " = "),
+                        new Token(Type.VALUE, "\\u1234", "\u1234"),
+                        new Token(Type.WHITESPACE, "\n\r"),
+                        new Token(Type.WHITESPACE, "  "),
+                        new Token(Type.COMMENT, "# final comment"));
     }
 
     @Test
@@ -100,6 +90,6 @@ public class TestPropertiesParser {
         StringReader rdr = new StringReader(props);
         Stream<Token> tokens = PropertiesParser.tokens(rdr);
         String props2 = tokens.map(Token::getRaw).collect(Collectors.joining());
-        assertThat(props2, equalTo(props));
+        assertThat(props2).isEqualTo(props);
     }
 }
