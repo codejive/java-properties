32509213ba6fd30b2f37dbaf67adf0009f05fc91
nx-eric-jarosch <Eric.Jarosch@nexus-ag.de>
2026-09-23 17:46:00 +0200

    fix: default encoding for test cases on windows (#72)
    `new String(byte[])` uses `Charset.defaultCharset()` as the encoding.
    This only works on machines where the default charset is UTF-8, because all of the test.properties files are encoded using UTF-8.
    
    Consequently, 21 tests fail on windows, because here the default charset is windows-1252, while the test.properties files are still encoded using UTF-8, causing a mismatch to be detected by the tests.
    
    The fix is to explicitly use the correct encoding.

--- src/test/java/org/codejive/properties/TestProperties.java
+++ src/test/java/org/codejive/properties/TestProperties.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.io.*;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -1126,6 +1127,6 @@ public class TestProperties {
     }
 
     private String readAll(Path f) throws IOException {
-        return new String(Files.readAllBytes(f));
+        return new String(Files.readAllBytes(f), StandardCharsets.UTF_8);
     }
 }
